475468ccde54f56d9348d22f42db85be4011d386
zhangce <tckxjzc@gamil.com>
2018-06-21 10:50:30 +0800

    更新以适用react 16.4

--- components/Bundle.tsx
+++ components/Bundle.tsx
@@ -1,47 +1,50 @@
 import React, {Component} from 'react';
 import Loading from './Loading';
-type Props={
-    el:Function
+
+type Props = {
+    el: Function
 };
-class Bundle extends Component<Props>{
-    state={
-        update:false,
-        Element:null
+
+class Bundle extends Component<Props> {
+    state = {
+        Element: null
     };
-    mounted:boolean;
+    mounted: boolean;
+
     constructor(props) {
         super(props);
     }
-    load(){
-        this.props.el().then((element)=>{
-            if(this.mounted)
-                this.setState({Element:element.default?element.default:element})
+
+    load() {
+        this.props.el().then((element) => {
+            if (this.mounted)
+                this.setState({Element: element.default ? element.default : element})
         })
     }
-    componentDidMount(){
-        this.mounted=true;
+
+    componentDidMount() {
+        this.mounted = true;
         this.load();
     }
 
-    componentWillUnmount(){
-        this.mounted=false;
+    componentWillUnmount() {
+        this.mounted = false;
     }
 
-    static getDerivedStateFromProps(nextProps, prevState){
-
-        return {update:true};
-    }
-    componentDidUpdate(){
-        if(this.state.update){
+    // static getDerivedStateFromProps(nextProps, prevState){
+    //
+    //     return {update:true};
+    // }
+    componentDidUpdate(prevProps) {
+        if (this.props.el != prevProps.el) {
             this.load();
-            this.setState({update:false});
         }
     }
 
     render() {
-        let {Element}=this.state;
+        let {Element} = this.state;
         return <div>
-            {Element?<Element/>:<Loading/>}
+            {Element ? <Element/> : <Loading/>}
         </div>
     }
 }
