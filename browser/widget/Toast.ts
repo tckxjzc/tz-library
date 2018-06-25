171b6258c9305c2a784c3cc76d96eb3853253f56
zhangce <tckxjzc@gamil.com>
2018-06-25 16:43:46 +0800

    change

--- browser/widget/Toast.ts
+++ browser/widget/Toast.ts
@@ -3,22 +3,8 @@ import LayerUiToast from "./LayerUiToast";
 let Proxy: any = LayerUiToast;
 
 class Toast {
-    toast;
-
     static makeText(msg: string) {
-        return new Toast(msg);
-    }
-
-    private constructor(msg: string) {
-        this.toast = new Proxy(msg);
-    }
-
-    setTime(time: number) {
-        return this.toast.setTime(time);
-    }
-
-    show() {
-        this.toast.show();
+        return new Proxy(msg);
     }
 
     static register(SubToast) {
